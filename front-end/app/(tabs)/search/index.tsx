a58561322d7443fcaa533ec5bcc48cfa3425b5e8
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-15 16:19:18 -0500

    Get Params in Search

--- front-end/app/(tabs)/search/index.tsx
+++ front-end/app/(tabs)/search/index.tsx
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-
+import { View, Text } from "react-native";
+import React from "react"; 
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useLocalSearchParams } from 'expo-router';
 const SearchScreen = () => {
+  const {query} = useLocalSearchParams<{query:string}>();
   return (
-<SafeAreaView>
-  
-</SafeAreaView>
-  )
-}
+    <SafeAreaView>
+      <View>
+        
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default SearchScreen
+export default SearchScreen;
