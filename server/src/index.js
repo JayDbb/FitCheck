081d220a9613f9df0c7f3a9c5c69e93fd9fa56a5
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-15 21:27:07 -0500

    Refactor index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,25 +4,20 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express()
-const port = 3000
 const routes = require('./routes');
-
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' })); // Increase JSON limit
-app.use(express.urlencoded({ limit: '50mb', extended: true })); // Increase URL-encoded form limit
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true })); 
 app.use(helmet());
 app.use(morgan('dev'));
-
-// Increase payload size limit
 app.use(routes);
 
-if(process.env.NODE_ENV !== 'test'){ // Only connect to DB in non-test environment
-
-  app.listen(port, () => {
-    console.log(`FitCheck+ Server listening on port ${port}`)
+if(process.env.NODE_ENV !== 'test'){ 
+  app.listen(PORT, () => {
+    console.log(`FitCheck+ Server listening on port ${PORT}`)
   })
-
 }
 
 module.exports = app;
